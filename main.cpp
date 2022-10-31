678f301bdf79d19aa1e033a4bce436d32dc043b7
phanhoanghai <phanhoanghai90@gmail.com>
2022-10-31 18:33:05 +0700

    init share mem

--- main.cpp
+++ main.cpp
@@ -1,11 +1,13 @@
 #include <iostream>
 #include "Controller/datamanager.h"
+#include "multiprocess/sharemem.h"
 
 using namespace std;
 
 int main()
 {
     DataManager::getInstance()->initDataFromJsonFile();
+    ShareMem::getInstance()->initShareMem();
     qDebug("%s", DataManager::getInstance()->getTextData().toStdString().c_str());
     return 0;
 }
